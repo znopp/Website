ad1bca63de62ba1019f9a9125b74125a2d80c8c9
znopp <niklas.termination@gmail.com>
2024-09-28 23:36:28 +0200

    Theme toggle

---
+++ js/darkmode.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+    var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+    var themeToggleBtn = document.getElementById('theme-toggle');
+
+    // Initialize icons based on current theme
+    if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        themeToggleLightIcon.classList.remove('hidden');
+    } else {
+        themeToggleDarkIcon.classList.remove('hidden');
+    }
+
+    // Add click event listener
+    themeToggleBtn.addEventListener('click', function() {
+        // Toggle icons
+        themeToggleDarkIcon.classList.toggle('hidden');
+        themeToggleLightIcon.classList.toggle('hidden');
+
+        // Toggle theme
+        if (localStorage.getItem('color-theme')) {
+            if (localStorage.getItem('color-theme') === 'light') {
+                document.documentElement.classList.add('dark');
+                localStorage.setItem('color-theme', 'dark');
+            } else {
+                document.documentElement.classList.remove('dark');
+                localStorage.setItem('color-theme', 'light');
+            }
+        } else {
+            if (document.documentElement.classList.contains('dark')) {
+                document.documentElement.classList.remove('dark');
+                localStorage.setItem('color-theme', 'light');
+            } else {
+                document.documentElement.classList.add('dark');
+                localStorage.setItem('color-theme', 'dark');
+            }
+        }
+    });
+});
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./index.html",
+    "./html/**/*.html",
+    "./js/**/*.js"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        accent: {
+          1: "hsl(255 35% 28%)",
+          2: "hsl(12 62% 56%)"
+        },
+        dark: {
+          1: "hsl(220 28% 22%)",
+          2: "hsl(222 28% 28%)",
+          3: "hsl(222 29% 34%)",
+          4: "hsl(222 29% 40%)",
+        },
+        light: {
+          1: "hsl(219 9% 78%)",
+          2: "hsl(219 7% 84%)",
+          3: "hsl(218 5% 90%)",
+          4: "hsl(218 3% 96%)"
+        }
+      }
+    }
+  },
+  plugins: [],
+}
